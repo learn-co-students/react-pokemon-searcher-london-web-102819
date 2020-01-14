5d7d903eda28a9c71d7627108251fe65d757ea6f
Faris Aziz <farisaziz12@gmail.com>
2020-01-14 09:59:55 +0000

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
-  render() {
+  state = {
+    displayBack: false
+  }
+
+  handleImageClick = () => {
+    this.setState({
+      displayBack: !this.state.displayBack
+    })
+  }
+
+
+  render() { 
+    const hp = this.props.stats.find(stat => (
+      stat.name === "hp"
+    ))
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div onClick={this.handleImageClick} className="image">
+            <img src={!this.state.displayBack? this.props.sprites.front :this.props.sprites.back} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp.value} hp
             </span>
           </div>
         </div>
