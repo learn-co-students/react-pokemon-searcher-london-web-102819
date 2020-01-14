6ac0a19a0fabe93e4f8f66211f90aa7cebd81b0a
Anastasia Pankina <siapankina@gmail.com>
2020-01-14 17:12:12 +0000

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    toggle: false
+  };
+  hp = this.props.pokemon.stats.find(p => p.name === "hp");
+
+  handleToggle = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
+
   render() {
     return (
       <Card>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              alt="oh no!"
+              src={
+                this.state.toggle
+                  ? this.props.pokemon.sprites.back
+                  : this.props.pokemon.sprites.front
+              }
+              onClick={this.handleToggle}
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.hp.value}
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
